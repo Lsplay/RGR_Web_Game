5cc8a98811b22f165765bd33d4754bbfb6f41443
Lsplay <64605879+Lsplay@users.noreply.github.com>
2021-05-30 18:05:26 +0400

    Add files via upload
    Контроллер под регистрацию(Пока не работающий из-за штмл) и админский контроллер , что пока что выводит базу данных со всеми игроками.

---
+++ pvp_knights/src/main/java/com/example/pvp_knights/Controllers/AdminMenuController.java
@@ -0,0 +1,41 @@
+package com.example.pvp_knights.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.pvp_knights.dataBase.data.user_information_service;
+
+@Controller
+public class AdminMenuController {
+
+	@Autowired
+	private user_information_service userService;
+
+	@GetMapping("/admin")
+	public String userList(Model model) {
+		model.addAttribute("allUsers", userService.allUsers());
+		return "admin";
+	}
+
+	@PostMapping("/admin")
+	public String deleteUser(@RequestParam(required = true, defaultValue = "") Long userId,
+			@RequestParam(required = true, defaultValue = "") String action, Model model) {
+
+		if(action.equals("delete")) {
+			userService.deleteUser(userId);
+		}
+		return "redirect:/admin";
+	}
+	
+	@GetMapping("/admin/gt/{userId}")
+	public String gtUser(@PathVariable("userId") Long userId, Model model) {
+		model.addAttribute("allUsers", userService.usergtList(userId));
+		return "admin";
+		
+	}
+}
---
+++ pvp_knights/src/main/java/com/example/pvp_knights/Controllers/RegController.java
@@ -0,0 +1,49 @@
+package com.example.pvp_knights.Controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.example.pvp_knights.dataBase.data.user_information_service;
+import com.example.pvp_knights.dataBase.models.user_information;
+
+@Controller
+public class RegController {
+
+	@Autowired
+	private user_information_service userService;
+
+	@GetMapping("/registration")
+	public String registration(Model model) {
+		model.addAttribute("userForm", new user_information());
+
+		return "registration";
+	}
+
+	@PostMapping("/registration")
+	public String addUser(@ModelAttribute("userForm") user_information userForm, BindingResult bindingResult,
+			Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
+		if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
+			model.addAttribute("passwordError", "Пароли не совпадают");
+			return "registration";
+		}
+		if (!userService.saveUser(userForm)) {
+			model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
+			return "registration";
+		}
+
+		return "redirect:/";
+	}
+
+}
