76c66f60658183a6ae7d644f3cec268685d32073
Lsplay <64605879+Lsplay@users.noreply.github.com>
2021-05-30 18:03:35 +0400

    Add files via upload
    Добавил spring Security , установил доступ к некоторым страницам для определенных ролей

---
+++ pvp_knights/src/main/java/com/example/pvp_knights/Config/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.pvp_knights.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.pvp_knights.dataBase.data.user_information_service;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+@Autowired
+	user_information_service userService;
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity
+			.csrf()
+				.disable()
+					.authorizeRequests()
+									//Только те, кто не зарегестрированны
+						.antMatchers("/registration").not().fullyAuthenticated() 
+						.antMatchers("/css/**", "/images/**","/js/**","/jquery/**","/fonts/**").permitAll()
+									//Админы
+						.antMatchers("/admin/**").hasRole("ADMIN")
+									//Обычные Пользователи
+						.antMatchers("/news").hasRole("USER")
+									//Все, кто авторизован
+						.antMatchers("/","/game/**").permitAll()
+						.anyRequest().authenticated()
+						.and()
+						//Для входа в систему
+								.formLogin()
+								.loginPage("/login")
+								//При успешном входе перенаправление на главную страницу
+								.defaultSuccessUrl("/").permitAll()
+						.and()
+								//Перенаправление при выходе
+								.logout()
+								.permitAll().logoutSuccessUrl("/");
+		
+	}
+	
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+}
